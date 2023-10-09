23fc5e77c3006c0302522cc166be90c5b104cb2a
Julius Eronen <julius23001@student.hamk.fi>
2023-10-09 16:28:01 +0300

    Initial commit

---
+++ src/TulostinOhjelma.java
@@ -0,0 +1,26 @@
+public class TulostinOhjelma {
+
+	public static void main(String[] args) {
+		
+		String tekija = "Julius";
+		double luku1 = 50;
+		double luku2 = 10;
+		double jako = luku1 / luku2;
+		
+		System.out.println("Hei olen Tulostin-ohjelma");
+		System.out.println("Ohjelman tekijä: " + tekija);
+		System.out.println("luku1-muuttujan arvo on " + luku1); 
+		System.out.println("luku2-muuttujan arvo on " + luku2);
+		System.out.print(luku1);
+		System.out.print(" / ");
+		System.out.print(luku2);
+		System.out.print(" = ");
+		System.out.println(jako);
+		
+		
+		
+		
+		
+	}
+
+}
